b117e266b6838b2d45678769d05cfb03b65cea98
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2025-02-24 21:29:56 +0000

    Aula 1.1

--- Aluno.java
+++ Aluno.java
@@ -1,12 +1,21 @@
 public class Aluno{
     public String nome;
     public double altura;
+    public int matricula;
     
+
+    public void spaceMenu(){
+        System.out.println(" ");
+    }
+
     public void estudar(String materia){
     System.out.println( nome + materia);
     
     }
     public void pedirNota(String nomeProfessor){
-    System.out.println( nome +"está pedindo nota a: " + nomeProfessor);
+    System.out.println( nome +" está pedindo nota a: " + nomeProfessor);
+    }
+    public void pedirMatricula(int matricula){
+        System.out.println("A matrícula de " + nome + " é: "+ matricula);
     }
 }
--- App.java
+++ App.java
@@ -3,17 +3,23 @@ public class App{
         Aluno aluno1 = new Aluno();
         aluno1.nome = "Gabriel";
         aluno1.altura = 1.70;
+        aluno1.matricula = 123;
+
         System.out.println("Nome: " + aluno1.nome);
+        /* spaceMenu();  */
         System.out.println("Altura: "+ aluno1.altura);
-        aluno1.estudar("POO");
+        aluno1.estudar(" deve estudar mais POO");
         aluno1.pedirNota("Iago");
+        aluno1.pedirMatricula(aluno1.matricula);
 
         Aluno aluno2 = new Aluno();
         aluno2.nome = "Delmiro";
-
         aluno2.altura = 1.69;
-        aluno2.estudar("Projeto Integrador");
+        aluno2.matricula = 456;
+        aluno2.estudar(" deve se preocupar mais com o Projeto Integrador");
         aluno2.pedirNota("Márcio");
+        aluno2.pedirMatricula(aluno2.matricula);
+        
 
     }
 }
